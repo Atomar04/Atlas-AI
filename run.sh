df17505484bd97349a288ccb71f7951b411970a5
Atomar04 <arpitatomar2004@gmail.com>
2026-04-11 15:05:57 +0530

    enabled backend CORS

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BACKEND_DIR="$ROOT_DIR/src/backend_dummy"
+BACKEND_DIR="$ROOT_DIR/src/backend"
 FRONTEND_DIR="$ROOT_DIR/src/frontend"
 
 cleanup() {
